19f29429e104842d3d7f165aa9b513bffeeba9fd
Igor <igor53c@gmail.com>
2023-08-18 17:05:24 +0200

    Add project files.

---
+++ LuhnChecksum/LuhnAlgorithm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LuhnCsharpImplementation
+{
+    public static class LuhnAlgorithm
+    {
+        public static bool IsValid(String number)
+        {
+            number = number.Replace(" ", "");
+
+            if (number.Length <= 1 || !IsAllDigits(number))
+            {
+                return false;
+            }
+
+            int sum = 0;
+            bool shouldDouble = false;
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                int digit = number[i] - '0';
+
+                if (shouldDouble)
+                {
+                    digit *= 2;
+
+                    if (digit > 9)
+                    {
+                        digit -= 9;
+                    }
+                }
+
+                sum += digit;
+                shouldDouble = !shouldDouble;
+            }
+
+            return sum % 10 == 0;
+        }
+
+        private static bool IsAllDigits(string number)
+        {
+            foreach (char c in number)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LuhnChecksum/LuhnAlgorithmTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xunit;
+using LuhnCsharpImplementation;
+
+namespace LuhnAlgorithmTests
+{
+    public class LuhnAlgorithmTest
+    {
+        [Fact]
+        public void TestIsValidWithSingleDigit()
+        {
+            Assert.False(LuhnAlgorithm.IsValid("1"));
+        }
+
+        [Fact]
+        public void TestIsValidWithValidThreeDigitNumber()
+        {
+            Assert.True(LuhnAlgorithm.IsValid("059"));
+        }
+
+        [Fact]
+        public void TestIsValidWithValidNumberContainingSpaces()
+        {
+            Assert.True(LuhnAlgorithm.IsValid("055 444 285"));
+        }
+
+        [Fact]
+        public void TestIsValidWithInvalidCharacter()
+        {
+            Assert.False(LuhnAlgorithm.IsValid("059a"));
+        }
+
+        [Fact]
+        public void TestIsValidWithSpecialCharacters()
+        {
+            Assert.False(LuhnAlgorithm.IsValid("055# 444$ 285"));
+        }
+
+        [Fact]
+        public void TestIsValidWithZeros()
+        {
+            Assert.True(LuhnAlgorithm.IsValid("0000 0"));
+        }
+        [Fact]
+        public void TestIsValidWithHyphens()
+        {
+            Assert.False(LuhnAlgorithm.IsValid("055-444-285"));
+        }
+    }
+}
